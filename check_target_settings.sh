f782e35148c01520ccf239ab7a76b729f0dabee5
Wen.Liao <zzz00072@gmail.com>
2013-10-03 23:12:22 +0800

    Rename dump file to target settings

--- check_target_settings.sh
+++ check_target_settings.sh
@@ -3,9 +3,9 @@ RESULT=.check_target_settings
 
 rm -f $RESULT
 
-HAS_DUMP_FILE=0
+HAS_TARGET_SETTINGS=0
 if [ -f "target_settings" ] ; then
-    HAS_DUMP_FILE=1
+    HAS_TARGET_SETTINGS=1
 fi
 
-echo "set variable \$has_dump_file=$HAS_DUMP_FILE" > $RESULT
+echo "set variable \$has_target_settings=$HAS_TARGET_SETTINGS" > $RESULT
